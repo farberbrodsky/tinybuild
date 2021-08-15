65728b3e173e5d044b672c6821297fdce4b583fa
Misha Farber Brodsky <misha@farberbrodsky.com>
2021-08-15 23:36:27 +0300

    Add base image reading

--- main.c
+++ main.c
@@ -23,11 +23,12 @@ int main(int argc, char *argv[], char *envp[]) {
     // remove whatever's after the last /
     size_t my_path_len = my_path_read_size;
     while (my_path[my_path_len] != '/') my_path_len--;
-    my_path[my_path_len] = '\0';
+    my_path[++my_path_len] = '\0';
 
-    char *tinybuild_private_dir = malloc(my_path_len + 12);
+    char *tinybuild_private_dir = malloc(my_path_len + strlen(".tinybuild/") + 1);
     strcpy(tinybuild_private_dir, my_path);
     strcat(tinybuild_private_dir, ".tinybuild/");
+    size_t tinybuild_private_dir_len = my_path_len + strlen(".tinybuild/");
 
     if (mkdir(tinybuild_private_dir, 0700) != 0) {  // create the .tinybuild directory and only my user can access it
         switch (errno) {
@@ -43,10 +44,23 @@ int main(int argc, char *argv[], char *envp[]) {
         }
     }
 
-    free(my_path);
-    free(tinybuild_private_dir);
+    if (getenv("FROM") == NULL) {
+        fputs("Must have FROM for base image, stored in .tinybuild/imagename\n", stderr);
+        return 1;
+    }
 
-    printf("FROM is %s\n", getenv("FROM"));
+    char *base_image_path = malloc(tinybuild_private_dir_len + strlen(getenv("FROM")) + 1);
+    memcpy(base_image_path, tinybuild_private_dir, tinybuild_private_dir_len);
+    memcpy(base_image_path + tinybuild_private_dir_len, getenv("FROM"), strlen(getenv("FROM")));
+    struct stat base_image_stat;
+    if (stat(base_image_path, &base_image_stat) != 0 || !S_ISDIR(base_image_stat.st_mode)) {
+        fprintf(stderr, "Image does not exist at %s\n", base_image_path);
+        return 1;
+    }
+
+    // great - we have a .tinybuild folder with a base image in it! now we can like, you know, make a container
+
+    /*
     // take all environment variables that start with INSTALL, in alphabetical order
     char **install = get_vars_with_prefix(envp, "INSTALL");
     char **install_p = install;
@@ -55,5 +69,9 @@ int main(int argc, char *argv[], char *envp[]) {
         ++install_p;
     }
     free(install);
+    */
+
+    free(my_path);
+    free(tinybuild_private_dir);
     return 0;
 }
